6da9e63a0152e28b72cfce30569ad33127720e38
JeffriLieca <jhandoyo@student.ciputra.ac.id>
2022-06-15 12:37:40 +0700

    form struk

---
+++ Projek Tea Break/FormStruk.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Projek_Tea_Break
+{
+    partial class FormStruk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStruk));
+            this.labelNetto = new System.Windows.Forms.Label();
+            this.labelBruto = new System.Windows.Forms.Label();
+            this.labelDiskon = new System.Windows.Forms.Label();
+            this.labelKetTot = new System.Windows.Forms.Label();
+            this.labelStrip = new System.Windows.Forms.Label();
+            this.labelketSub = new System.Windows.Forms.Label();
+            this.labelKetDis = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.labelUrutan = new System.Windows.Forms.Label();
+            this.labelCustomer = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.labelIDNota = new System.Windows.Forms.Label();
+            this.labelKasir = new System.Windows.Forms.Label();
+            this.labelJam = new System.Windows.Forms.Label();
+            this.labelTanggal = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNetto
+            // 
+            this.labelNetto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelNetto.Location = new System.Drawing.Point(299, 735);
+            this.labelNetto.Name = "labelNetto";
+            this.labelNetto.Size = new System.Drawing.Size(84, 23);
+            this.labelNetto.TabIndex = 45;
+            this.labelNetto.Text = "80,000";
+            this.labelNetto.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelBruto
+            // 
+            this.labelBruto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelBruto.Location = new System.Drawing.Point(299, 679);
+            this.labelBruto.Name = "labelBruto";
+            this.labelBruto.Size = new System.Drawing.Size(84, 23);
+            this.labelBruto.TabIndex = 44;
+            this.labelBruto.Text = "100,000";
+            this.labelBruto.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelDiskon
+            // 
+            this.labelDiskon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelDiskon.Location = new System.Drawing.Point(299, 707);
+            this.labelDiskon.Name = "labelDiskon";
+            this.labelDiskon.Size = new System.Drawing.Size(84, 23);
+            this.labelDiskon.TabIndex = 43;
+            this.labelDiskon.Text = "20,000";
+            this.labelDiskon.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelKetTot
+            // 
+            this.labelKetTot.AutoSize = true;
+            this.labelKetTot.Location = new System.Drawing.Point(245, 735);
+            this.labelKetTot.Name = "labelKetTot";
+            this.labelKetTot.Size = new System.Drawing.Size(48, 17);
+            this.labelKetTot.TabIndex = 42;
+            this.labelKetTot.Text = "Total :";
+            // 
+            // labelStrip
+            // 
+            this.labelStrip.Location = new System.Drawing.Point(42, 662);
+            this.labelStrip.Name = "labelStrip";
+            this.labelStrip.Size = new System.Drawing.Size(348, 17);
+            this.labelStrip.TabIndex = 41;
+            this.labelStrip.Text = " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " +
+    "- - - - -";
+            // 
+            // labelketSub
+            // 
+            this.labelketSub.AutoSize = true;
+            this.labelketSub.Location = new System.Drawing.Point(225, 679);
+            this.labelketSub.Name = "labelketSub";
+            this.labelketSub.Size = new System.Drawing.Size(68, 17);
+            this.labelketSub.TabIndex = 40;
+            this.labelketSub.Text = "Subtotal :";
+            // 
+            // labelKetDis
+            // 
+            this.labelKetDis.AutoSize = true;
+            this.labelKetDis.Location = new System.Drawing.Point(234, 707);
+            this.labelKetDis.Name = "labelKetDis";
+            this.labelKetDis.Size = new System.Drawing.Size(59, 17);
+            this.labelKetDis.TabIndex = 39;
+            this.labelKetDis.Text = "Diskon :";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(42, 115);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(344, 17);
+            this.label15.TabIndex = 38;
+            this.label15.Text = "==========================================";
+            // 
+            // labelUrutan
+            // 
+            this.labelUrutan.Location = new System.Drawing.Point(195, 275);
+            this.labelUrutan.Name = "labelUrutan";
+            this.labelUrutan.Size = new System.Drawing.Size(51, 17);
+            this.labelUrutan.TabIndex = 37;
+            this.labelUrutan.Text = "Urutan";
+            this.labelUrutan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCustomer
+            // 
+            this.labelCustomer.Location = new System.Drawing.Point(109, 248);
+            this.labelCustomer.Name = "labelCustomer";
+            this.labelCustomer.Size = new System.Drawing.Size(222, 17);
+            this.labelCustomer.TabIndex = 36;
+            this.labelCustomer.Text = "Nama Pemesan";
+            this.labelCustomer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(42, 219);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(344, 17);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "==========================================";
+            // 
+            // labelIDNota
+            // 
+            this.labelIDNota.AutoSize = true;
+            this.labelIDNota.Location = new System.Drawing.Point(125, 192);
+            this.labelIDNota.Name = "labelIDNota";
+            this.labelIDNota.Size = new System.Drawing.Size(88, 17);
+            this.labelIDNota.TabIndex = 34;
+            this.labelIDNota.Text = "2206120007";
+            // 
+            // labelKasir
+            // 
+            this.labelKasir.AutoSize = true;
+            this.labelKasir.Location = new System.Drawing.Point(125, 175);
+            this.labelKasir.Name = "labelKasir";
+            this.labelKasir.Size = new System.Drawing.Size(75, 17);
+            this.labelKasir.TabIndex = 33;
+            this.labelKasir.Text = "Kei Patrick";
+            // 
+            // labelJam
+            // 
+            this.labelJam.AutoSize = true;
+            this.labelJam.Location = new System.Drawing.Point(125, 158);
+            this.labelJam.Name = "labelJam";
+            this.labelJam.Size = new System.Drawing.Size(44, 17);
+            this.labelJam.TabIndex = 32;
+            this.labelJam.Text = "12.50";
+            // 
+            // labelTanggal
+            // 
+            this.labelTanggal.AutoSize = true;
+            this.labelTanggal.Location = new System.Drawing.Point(125, 141);
+            this.labelTanggal.Name = "labelTanggal";
+            this.labelTanggal.Size = new System.Drawing.Size(117, 17);
+            this.labelTanggal.TabIndex = 31;
+            this.labelTanggal.Text = "11 Februari 2003";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(54, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 17);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "ID Nota  :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Kasir :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Jam :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Tanggal :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "teabreak@gmail.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 17);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Telp : 081157793714";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(91, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 65);
+            this.pictureBox1.TabIndex = 46;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormStruk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 767);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelNetto);
+            this.Controls.Add(this.labelBruto);
+            this.Controls.Add(this.labelDiskon);
+            this.Controls.Add(this.labelKetTot);
+            this.Controls.Add(this.labelStrip);
+            this.Controls.Add(this.labelketSub);
+            this.Controls.Add(this.labelKetDis);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.labelUrutan);
+            this.Controls.Add(this.labelCustomer);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.labelIDNota);
+            this.Controls.Add(this.labelKasir);
+            this.Controls.Add(this.labelJam);
+            this.Controls.Add(this.labelTanggal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormStruk";
+            this.Text = "FormStruk";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNetto;
+        private System.Windows.Forms.Label labelBruto;
+        private System.Windows.Forms.Label labelDiskon;
+        private System.Windows.Forms.Label labelKetTot;
+        private System.Windows.Forms.Label labelStrip;
+        private System.Windows.Forms.Label labelketSub;
+        private System.Windows.Forms.Label labelKetDis;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label labelUrutan;
+        private System.Windows.Forms.Label labelCustomer;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label labelIDNota;
+        private System.Windows.Forms.Label labelKasir;
+        private System.Windows.Forms.Label labelJam;
+        private System.Windows.Forms.Label labelTanggal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
